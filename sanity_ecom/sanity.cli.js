7f98440d92bfc7f41d5608d8b47a295f8f3266fc
Kaushik Anubhav <kausanubhav2018@gmail.com>
2023-02-02 04:42:18 +0530

    last

---
+++ sanity_ecom/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zkrlum6h',
+    dataset: 'production'
+  }
+})
---
+++ sanity_ecom/schemas/index.js
@@ -0,0 +1,4 @@
+import product from './product.js'
+import banner from './banner'
+
+export const schemaTypes = [product, banner]
