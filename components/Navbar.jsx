abaf6b3fb232ffbc74b96c20e23af63b9005954e
Kaushik Anubhav <kausanubhav2018@gmail.com>
2023-02-02 15:48:11 +0530

    lt

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Szone Headphones</Link>
+        <Link href="/">Szone</Link>
       </p>
 
       <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
